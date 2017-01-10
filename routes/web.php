6b7e5cc79643097407af2ceef8629ecdfd0611e5
joanandreu <ja@iMac.local>
2017-01-10 02:31:00 +0100

    recolocar les rutes
    per access nomes admin s’han de caviar les rites de lloc

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,13 @@ Route::group(['namespace' => 'Backend', 'prefix' => 'admin', 'as' => 'admin.', '
      */
     includeRouteFiles(__DIR__.'/Backend/');
 });
+
+
+Route::get('admin/entidads', ['as'=> 'admin.entidads.index', 'uses' => 'Admin\EntidadController@index']);
+Route::post('admin/entidads', ['as'=> 'admin.entidads.store', 'uses' => 'Admin\EntidadController@store']);
+Route::get('admin/entidads/create', ['as'=> 'admin.entidads.create', 'uses' => 'Admin\EntidadController@create']);
+Route::put('admin/entidads/{entidads}', ['as'=> 'admin.entidads.update', 'uses' => 'Admin\EntidadController@update']);
+Route::patch('admin/entidads/{entidads}', ['as'=> 'admin.entidads.update', 'uses' => 'Admin\EntidadController@update']);
+Route::delete('admin/entidads/{entidads}', ['as'=> 'admin.entidads.destroy', 'uses' => 'Admin\EntidadController@destroy']);
+Route::get('admin/entidads/{entidads}', ['as'=> 'admin.entidads.show', 'uses' => 'Admin\EntidadController@show']);
+Route::get('admin/entidads/{entidads}/edit', ['as'=> 'admin.entidads.edit', 'uses' => 'Admin\EntidadController@edit']);
