ce84ee6baf4ea60d249300362dfbbbae70335a06
joanandreu <joan@imov.local>
2017-01-12 15:23:21 +0100

    disseny del menú del sidebar ( inacabat)

--- resources/views/backend/includes/sidebar.blade.php
+++ resources/views/backend/includes/sidebar.blade.php
@@ -13,15 +13,33 @@
                     <span>{{ trans('menus.backend.sidebar.dashboard') }}</span>
                 </a>
             </li>
-
+            <li class="{{ Active::pattern('admin/dashboard') }}">
+                <a href="{{ route('admin.dashboard') }}">
+                    <i class="fa fa-cubes"></i>
+                    <span>Gestión de Entidades</span>
+                </a>
+            </li>
+            <li class="{{ Active::pattern('admin/dashboard') }}">
+                <a href="{{ route('admin.dashboard') }}">
+                    <i class="fa fa-exclamation-triangle "></i>
+                    <span>Gestión de Riesgos</span>
+                </a>
+            </li>
+            <li class="{{ Active::pattern('admin/dashboard') }}">
+                <a href="{{ route('admin.dashboard') }}">
+                    <i class="fa fa-file-text-o"></i>
+                    <span>Gestión de Planes</span>
+                </a>
+            </li>
            
+
 
             <li class="header">{{ trans('menus.backend.sidebar.system') }}</li>
             
             @permissions(['manage-users', 'manage-roles'])
                 <li class="{{ Active::pattern('admin/access/*') }} treeview">
                     <a href="#">
-                        <i class="fa fa-users"></i>
+                        <i class="fa fa-key"></i>
                         <span>{{ trans('menus.backend.access.title') }}</span>
                         <i class="fa fa-angle-left pull-right"></i>
                     </a>
@@ -55,17 +73,30 @@
                     <i class="fa fa-angle-left pull-right"></i>
                 </a>
                 <ul class="treeview-menu {{ Active::pattern('admin/log-viewer*', 'menu-open') }}" style="display: none; {{ Active::pattern('admin/log-viewer*', 'display: block;') }}">
-                    <li class="{{ Active::pattern('admin/log-viewer') }}">
-                        <a href="{{ route('admin.log-viewer::dashboard') }}">
-                            <i class="fa fa-circle-o"></i>
-                            <span>{{ trans('menus.backend.log-viewer.dashboard') }}</span>
-                        </a>
-                    </li>
+                    
 
                     <li class="{{ Active::pattern('admin/log-viewer/logs') }}">
                         <a href="{{ route('admin.log-viewer::logs.list') }}">
                             <i class="fa fa-circle-o"></i>
                             <span>{{ trans('menus.backend.log-viewer.logs') }}</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+
+                        <li class="{{ Active::pattern('admin/log-viewer*') }} treeview">
+                <a href="#">
+                    <i class="fa fa-database"></i>
+                    <span>Administración de la BD</span>
+                    <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                <ul class="treeview-menu {{ Active::pattern('admin/log-viewer*', 'menu-open') }}" style="display: none; {{ Active::pattern('admin/log-viewer*', 'display: block;') }}">
+                    
+
+                    <li class="{{ Active::pattern('admin/log-viewer/logs') }}">
+                        <a href="{{ route('admin.log-viewer::logs.list') }}">
+                            <i class="fa fa-table"></i>
+                            <span>Variables</span>
                         </a>
                     </li>
                 </ul>
