62b43c7e7d9d33531411b9182aeaa5572b6e1bc4
joanandreu <joan@imov.local>
2017-01-16 19:19:21 +0100

    canvis menors

--- resources/views/backend/tipocargos/create.blade.php
+++ resources/views/backend/tipocargos/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <section class="content-header">
         <h1>
-            Tipocargo
+            Alta Cargo
         </h1>
     </section>
     <div class="content">
--- resources/views/backend/tipocargos/show.blade.php
+++ resources/views/backend/tipocargos/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <section class="content-header">
         <h1>
-            Tipo Cargo
+            Cargo
         </h1>
     </section>
     <div class="content">
